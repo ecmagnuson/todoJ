e23f63eef186269061e14abac9e78092d8e37cf7
Elliott <ecmagnuson@wisc.edu>
2022-10-06 23:52:24 -0500

    tested import and usage of commons-cli

--- src/main/java/j/todo/TodoJ.java
+++ src/main/java/j/todo/TodoJ.java
@@ -1,13 +1,26 @@
 package j.todo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class TodoJ {
+    public static void main(String[] args) throws ParseException {
+	// create Options object
+	Options options = new Options();
+	// add t option
+	options.addOption("t", false, "display current time");
+
+	CommandLineParser parser = new DefaultParser();
+	CommandLine cmd = parser.parse(options, args);
+
+	if (cmd.hasOption("t")) {
+	    System.out.println("T");
+	} else {
+	    System.out.println("F");
+	}
+
     }
 }
