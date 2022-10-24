8925bb70c4420ebbda0303935f094ce80499b156
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-10-24 10:27:37 -0500

    adding options WIP

--- src/main/java/j/todo/TodoJ.java
+++ src/main/java/j/todo/TodoJ.java
@@ -6,21 +6,49 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import java.io.IOException;
+import java.io.File;
+import java.util.Arrays;
+
 public class TodoJ {
-    public static void main(String[] args) throws ParseException {
-	// create Options object
-	Options options = new Options();
-	// add t option
-	options.addOption("t", false, "display current time");
-
-	CommandLineParser parser = new DefaultParser();
-	CommandLine cmd = parser.parse(options, args);
-
-	if (cmd.hasOption("t")) {
-	    System.out.println("T");
-	} else {
-	    System.out.println("F");
+	
+	private static Options setupOptions() {
+		Options options = new Options();
+		options.addOption("a", false, "add an item to the text file.");
+		options.addOption("d", false, "remove an item from the text file.");
+		options.addOption("lsc", false, "list out all of the contexts.");
+		
+		
+		return options;
+	}
+	
+	public static void createTextFile(String path, String name) throws IOException {
+		File f = new File(path + name);
+		f.getParentFile().mkdirs();
+		f.createNewFile();
 	}
 
-    }
+	public static void main(String[] args) throws ParseException {
+		Options options = setupOptions();
+		CommandLineParser parser = new DefaultParser();
+		CommandLine line = parser.parse(options, args);
+		
+
+		if (line.hasOption("a")) {
+			System.out.println("A has been called");
+			String path = System.getProperty("user.dir");
+			try {
+				createTextFile(path + "/Test/", "file.txt");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("File added.");
+		} else {
+			System.out.println("else called");
+		}
+
+		System.out.println(Arrays.toString(line.getOptions()));
+
+	}
 }
