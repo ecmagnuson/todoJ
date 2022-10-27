38a4a0c52a8e9034472a95657fe8ab75d018a1ff
Elliott <ecmagnuson@wisc.edu>
2022-10-26 22:06:25 -0500

    added help

--- src/main/java/j/todo/TodoJ.java
+++ src/main/java/j/todo/TodoJ.java
@@ -1,56 +1,55 @@
 package j.todo;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import java.io.IOException;
-import java.io.File;
-import java.util.Arrays;
-
 public class TodoJ {
+
+    public static void createTextFile(String path, String name) throws IOException {
+	File f = new File(path + name);
+	f.getParentFile().mkdirs();
+	f.createNewFile();
+    }
+
+    private static void help() {
+	HelpFormatter formatter = new HelpFormatter();
+	formatter.printHelp("", "", setupOptions(), "", true);
+    }
+
+    private static Options setupOptions() {
+	Options options = new Options();
+	options.addOption("a", false, "add an item to the text file.");
+	options.addOption("d", false, "remove an item from the text file.");
+	options.addOption("lsc", false, "list out all of the contexts.");
+	options.addOption("ls", false, "list out all of the items to do.");
+	options.addOption("lsd", false, "list out all of the done objects.");
+	options.addOption("lsda", false, "list out all of the done objects with specific context.");
+	options.addOption("h", false, "list out the options and what they do.");
+	return options;
+    }
+
+    public static void main(String[] args) throws ParseException, IOException {
+	Options options = setupOptions();
+	CommandLineParser parser = new DefaultParser();
+	CommandLine line = parser.parse(options, args);
+	String path = System.getProperty("user.dir");
+
+	help();
 	
-	private static Options setupOptions() {
-		Options options = new Options();
-		options.addOption("a", false, "add an item to the text file.");
-		options.addOption("d", false, "remove an item from the text file.");
-		options.addOption("lsc", false, "list out all of the contexts.");
-		options.addOption("ls", false, "list out all of the items to do.");
-		options.addOption("lsd", false, "list out all of the done objects.");
-		options.addOption("lsda", false, "list out all of the done objects with specific context.");
-		options.addOption("h", false, "list out the options and what they do.");
-		return options;
+	if (line.hasOption("a")) {
+	    createTextFile(path, "name");
 	}
 	
-	public static void createTextFile(String path, String name) throws IOException {
-		File f = new File(path + name);
-		f.getParentFile().mkdirs();
-		f.createNewFile();
-	}
 
-	public static void main(String[] args) throws ParseException {
-		Options options = setupOptions();
-		CommandLineParser parser = new DefaultParser();
-		CommandLine line = parser.parse(options, args);
-		
+	System.out.println(Arrays.toString(line.getOptions()));
 
-		if (line.hasOption("a")) {
-			System.out.println("A has been called");
-			String path = System.getProperty("user.dir");
-			try {
-				createTextFile(path + "/Test/", "file.txt");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("File added.");
-		} else {
-			System.out.println("else called");
-		}
-
-		System.out.println(Arrays.toString(line.getOptions()));
-
-	}
+    }
 }
